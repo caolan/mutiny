919e203e3476c379ef724d349e0eb3cf822b560b
Milan <bits@fuckevil.org>
2024-08-02 23:25:31 -0500

    refactor: add check for unix socket
    If a socket file exists, and is not in use, then reuse it. Otherwise
    exit with an error.
    
    Issue: https://github.com/caolan/mutiny/issues/31

--- mutinyd/src/main.rs
+++ mutinyd/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, path::PathBuf};
+use std::{fs, os::unix::net::UnixStream, path::PathBuf, process};
+
 use clap::Parser;
 
 mod protocol;
@@ -24,13 +25,50 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
+    println!("Starting server...");
+
     let args = Args::parse();
-    let data_dir = args.data.unwrap_or_else(|| dirs::open_app_data_dir().unwrap());
-    let socket_path = args.socket.unwrap_or_else(|| dirs::open_app_runtime_dir().unwrap().join("mutinyd.socket"));
+    println!("Parsed arguments: {:?}", args);
+
+    let data_dir = args.data.unwrap_or_else(|| {
+        println!("No data directory specified, using default.");
+        dirs::open_app_data_dir().unwrap()
+    });
+    println!("Data directory: {:?}", data_dir);
+
+    let socket_path = args.socket.unwrap_or_else(|| {
+        println!("No socket specified, using default.");
+        dirs::open_app_runtime_dir().unwrap().join("mutinyd.socket")
+    });
+    println!("Socket path: {:?}", socket_path);
+
     let keypair_path = data_dir.join("identity.key");
     let db_path = data_dir.join("data.db");
-    // Ensure data directory exists
-    fs::create_dir_all(data_dir).unwrap();
-    let config = config::Config::load(keypair_path, socket_path, db_path).unwrap();
+
+    if !data_dir.exists() {
+        fs::create_dir_all(&data_dir).unwrap();
+        println!("Created data directory: {:?}", data_dir);
+    }
+    println!("Database path: {:?}", db_path);
+
+    if socket_path.exists() {
+        match UnixStream::connect(&socket_path) {
+            Ok(_) => {
+                eprintln!("Error: Socket is already in use: {:?}", socket_path);
+                process::exit(1); // Exit with an error code
+            }
+            Err(_) => {
+                println!("Reusing unix socket: {:?}", socket_path);
+                fs::remove_file(&socket_path).unwrap();
+            }
+        }
+    }
+
+    let config = config::Config::load(
+        keypair_path.clone(),
+        socket_path.clone(),
+        db_path.clone()
+    ).unwrap();
+
     server::Server::start(config).await.unwrap();
 }
